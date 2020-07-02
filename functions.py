411184de06b0b8dada71146c445bcbf1b4096b2c
JackBosley <jb2628@bath.ac.uk>
2020-07-02 22:48:32 +0100

    Added Harris corner finding Added crude feature matching between successive images

---
+++ functions.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  1 17:40:45 2020
+
+@author: Jack
+"""
+
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from scipy import signal
+import scipy.ndimage.filters as filters
+from scipy.optimize import minimize
+
+# For finding derivatives in images
+#------------------------------------------------------------------------------
+def get_dx(image):
+    im_left  = np.array(image.transform(image.size, Image.AFFINE, 
+                                        (1, 0, -1, 0, 1, 0)), dtype=np.int8)
+    im_right = np.array(image.transform(image.size, Image.AFFINE, 
+                                        (1, 0,  1, 0, 1, 0)), dtype=np.int8)
+
+    return np.subtract(im_right, im_left).astype(np.int32)
+
+def get_dy(image):
+    im_up    = np.array(image.transform(image.size, Image.AFFINE, 
+                                        (1, 0, 0, 0, 1,  1)), dtype=np.int8)
+    im_down  = np.array(image.transform(image.size, Image.AFFINE, 
+                                        (1, 0, 0, 0, 1, -1)), dtype=np.int8)
+
+    return np.subtract(im_up, im_down).astype(np.int32)
+
+
+
+# Finding common features
+#------------------------------------------------------------------------------                 
+def get_corners_fast(image):
+    d = 8
+    d2 = d * 2
+    k = 0.04
+    
+    width = image.width; height = image.height
+    
+    # Compute the aperture function
+    X = np.linspace(-d, d, num=(2*d)+1)
+    XX, YY = np.meshgrid(X, X)
+    w = np.exp(-((XX**2) + (YY**2)) / (2 * ((d / 2)**2)))
+    
+    # Get image gradients
+    I_x = get_dx(image)
+    I_y = get_dy(image)
+    
+    # Compute squares of gradients 
+    I_xx = np.square(I_x)
+    I_yy = np.square(I_y)
+    I_xy = np.multiply(I_x, I_y)
+    
+    # Structure tensor elements can be found by convolving
+    #  squared gradients with aperture function
+    A_11 = signal.convolve2d(I_xx, w, mode='same')
+    A_12 = signal.convolve2d(I_xy, w, mode='same')
+    A_22 = signal.convolve2d(I_yy, w, mode='same')
+
+    # Compute harris response
+    detM = np.multiply(A_11, A_22) - np.square(A_12)
+    trM = np.add(A_11, A_12)
+    M = detM - k * np.square(trM)
+        
+    # Find the local maxima
+    max_values = filters.maximum_filter(M, d2)
+    rows, cols = np.where(M == max_values)
+    points = list(zip(rows, cols))
+
+    # Compute normalised weights for each point
+    weights = M[np.where(M == max_values)]
+    distance_to_edge = 1 - ((np.abs(cols - (width / 2)) / width) +
+                            (np.abs(rows - (height / 2)) / height))
+    weights = np.multiply(weights, distance_to_edge)
+    weights = np.divide(weights, np.max(weights))
+     
+    # Sort points by weights
+    points_sorted = [p for _,p in reversed(sorted(zip(weights, points)))]
+    weights_sorted = list(reversed(sorted(weights)))
+    
+    return points_sorted[:20], weights_sorted[:20]
+        
+
+
+def compare(features_1, features_2):
+    def fit_score(delta):
+        dx, dy = delta
+        
+        SSR = 0
+        for p1, w1 in zip(points_1, weights_1):
+            DX = np.subtract(points_2[:][0], (p1[0] + dx))
+            DY = np.subtract(points_2[:][1], (p1[1] + dy))
+            
+            R_sq = np.add(DX**2, DY**2)
+            R_sq_min = np.min(R_sq)
+            SSR += R_sq_min * w1  
+        
+        for p2, w2 in zip(points_2, weights_2):
+            DX = np.subtract(points_1[:][0], (p2[0] - dx))
+            DY = np.subtract(points_1[:][1], (p2[1] - dy))
+            
+            R_sq = np.add(DX**2, DY**2)
+            R_sq_min = np.min(R_sq)
+            SSR += R_sq_min * w2
+        
+        return SSR
+    
+    points_1, weights_1 = features_1  
+    points_2, weights_2 = features_2
+    
+    bnds = ((-500, 500), (-500, 500))
+    res = minimize(fit_score, [0, 0], bounds=bnds, method='SLSQP')
+    
+    dy, dx = res.x
+    x1, y1 = zip(*points_1)
+    x2, y2 = zip(*points_2)
+    
+    plt.scatter(np.add(y1, -dy), np.add(x1, -dx))
+    plt.scatter(y2, x2)
+    plt.axis('equal')
+    
+    print(res.x)
+    return res.x
+    
+    
+    
+## WORKS BUT VERY SLOW
+def get_corners_true(image):
+    d = 5
+    k = 0.04
+    
+    def w():
+        X = np.linspace(-d, d, num=(2*d)+1)
+        XX, YY = np.meshgrid(X, X)
+        return np.exp(-((XX**2) + (YY**2)) / (2 * ((d / 2)**2)))
+    
+    @np.vectorize
+    def M(x, y):
+        # Harris response function
+        
+        # Get region around current pixel
+        _I_x = I_x[x-d-1:x+d, y-d-1:y+d]
+        _I_y = I_y[x-d-1:x+d, y-d-1:y+d]
+        
+        # Structure tensor elements are averaged, weighted, local gradient values
+        A_11 = np.sum(np.multiply(w, np.square(_I_x)))
+        A_12 = np.sum(np.multiply(w, np.multiply(_I_x, _I_y)))
+        A_22 = np.sum(np.multiply(w, np.square(_I_y)))
+        
+        # Harris response is smallest eigenvalue of tensor element,
+        #  approximately det(A) - k*Tr(A)^2
+        return ((A_11 * A_22) - (A_12 ** 2)) - (k * ((A_11 + A_22) ** 2))
+    
+    # Harris corner detection
+    
+    # Find derivatives
+    I_x = get_dx(image)
+    I_y = get_dy(image)  
+    
+    # Get aperture function
+    w = w()
+    
+    # Meshgrid the image for parallel computation
+    x, y = np.shape(image)
+    X, Y = [i+d+1 for i in range(x-(2*d)-1)],[i+d+1 for i in range(y-(2*d)-1)]
+    XX, YY = np.meshgrid(X, Y)
+    
+    # Get the Harris response
+    M = M(XX, YY)
+    
+    # Normalise and return as an image
+    M = np.divide(M, np.max(M) / 255)
+    return Image.fromarray(M.T)
---
+++ main.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  1 16:55:51 2020
+
+@author: Jack
+"""
+
+import os
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+
+import time
+
+from functions import compare, get_corners_fast
+
+
+def impose_features(feature_points, image, scale_factor = 1, directory = "", name="Corners"):
+    points, weights = feature_points
+    rows, cols = zip(*points)
+    
+    d = ImageDraw.Draw(image)
+    fnt = ImageFont.truetype("arial.ttf", 40)
+    dot_size = 24
+    for i, data in enumerate(zip(rows, cols, weights)):
+        r_s, c_s, w = data
+        
+        r = scale_factor * r_s; c = scale_factor * c_s
+        colour = (int(w * 255), 0, int((1 - w) * 255))
+        dot_size = (w * 24) + 5
+        
+        d.line([(c-dot_size, r-dot_size), (c+dot_size, r+dot_size)], fill=colour, width=5)
+        d.line([(c-dot_size, r+dot_size), (c+dot_size, r-dot_size)], fill=colour, width=5)
+        
+        d.text((c, r + 10), str(i), font=fnt)
+    
+    image.save(((directory + "/") if directory != "" else "") + (name + ".bmp"))
+    image.close()
+    
+    
+def main():
+    # Iterate through pictures
+    directory = "Photos2"
+    scale_factor = 12   
+    
+    start = time.time()
+    
+    fp1, fp2 = "", ""
+    for i, name in enumerate(os.listdir(directory)):
+        # If the first image's features have been found, reassign to fp2
+        if fp1 != "":
+            fp2 = fp1
+        
+        # Open current image and scale down for speed
+        image = Image.open(directory + "/" + name)
+        image_scaled = image.resize((image.width // scale_factor, 
+                                     image.height // scale_factor)).convert("L")
+        
+        # Get the locations of the features
+        fp1 = get_corners_fast(image_scaled)
+        #impose_features(fp1, image, scale_factor, "", "Corners" + str(i))
+        
+        comp_start = time.time()
+        # If the second image's features have been found,
+        #  compare current with previous features
+        if fp2 != "":
+            print(np.multiply(compare(fp1, fp2), scale_factor))
+            
+    end = time.time()
+    print("Corner Detection %f\nFeature Matching %f\nTotal %f" % (comp_start - start, end - comp_start, end - start))
+    
+            
+if __name__ == '__main__':
+    main()
