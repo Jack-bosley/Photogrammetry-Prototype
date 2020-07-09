ac2328d67fd481ecd2700b3e836ce7ddf7e654f7
JackBosley <jb2628@bath.ac.uk>
2020-07-09 18:11:01 +0100

    Hand calculated the Jacobian finder

--- src/Reconstruction/camera_model.py
+++ src/Reconstruction/camera_model.py
@@ -5,8 +5,8 @@ Created on Tue Jul  7 19:34:35 2020
 @author: Jack
 """
 
-import numpy as np
-
+import autograd.numpy as np
+import matplotlib.pyplot as plt
 
 # Class containing pre-calibrated camera information and camera specific methods
 class Camera:
@@ -25,23 +25,65 @@ class Camera:
         self.w, self.h = w, h
     
     
-    # Get camera rotation matrix from Euler angles
-    @staticmethod
-    def rotation_matrix(pitch, roll, yaw):   
-        # yaw
-        cy, sy = np.cos(roll), np.sin(roll)
+    # Returns pixel coordinates of projection of X with camera at T
+    def project(self, X, T):
+                
+        # Get camera properties
+        R_camera = Camera.rotation_matrix(T[0], T[1], T[2])
+        T_camera = np.array([T[3], T[4], T[5]])
         
-        # pitch
-        cp, sp = np.cos(yaw), np.sin(yaw)
         
-        # roll
-        cr, sr = np.cos(pitch), np.sin(pitch)
+        # Translate into camera reference frames
+        X_camera = np.matmul(R_camera, np.subtract(X, T_camera))
+        
+
+        # Project onto camera plane
+        u_norm_camera = np.array([self.f_x * X_camera[0] / X_camera[2], 
+                                  self.f_y * X_camera[1] / X_camera[2]])
+        
+            
+        # Distortions
+        u_norm_camera_r_sq = np.square(np.add(np.square(u_norm_camera[0]), 
+                                              np.square(u_norm_camera[1])))         
+
+        #  Radial
+        u_radial_correction = (self.k1 * u_norm_camera_r_sq +
+                               self.k2 * np.power(u_norm_camera_r_sq, 2) +
+                               self.k3 * np.power(u_norm_camera_r_sq, 3))
+        u_c_r = np.array([u_norm_camera[0] * u_radial_correction, 
+                          u_norm_camera[1] * u_radial_correction])
+        #  Tangential
+        u_c_t = np.array([(2*self.p1*u_norm_camera[0]*u_norm_camera[1]) + 
+                          (self.p2*(u_norm_camera_r_sq + (2*np.square(u_norm_camera[0])))),
+                          (self.p1*(u_norm_camera_r_sq + (2*np.square(u_norm_camera[1])))) + 
+                          (2*self.p2*u_norm_camera[0]*u_norm_camera[1])])
     
-        # Compute and return the rotation matrix
-        return np.array([[cy*cp, (cy*sp*sr) - (sy*cr), (cy*sp*cr) + (sy*sr)], 
-                         [sy*cp, (sy*sp*sr) + (cy*cr), (sy*sp*cr) - (cy*sr)], 
-                         [  -sp,                cp*sr,                cp*cr]])   
+        #  Apply distortions
+        u_norm_camera = np.array([u_norm_camera[0] + u_c_r[0] + u_c_t[0],
+                                  u_norm_camera[1] + u_c_r[1] + u_c_t[1]])
+            
+            
+        # Project into pixel plane
+        p_image = np.array([self.w * (u_norm_camera[0] + 0.5),
+                            self.h * (u_norm_camera[1] + 0.5)])
     
+        return p_image
+
+
+    def reproject_all(self, X, T, plot_reprojection=False):
+        reprojection = np.array([[self.project(X, T) for X in X] for T in T])
+             
+        # Plot points for debugging purposes
+        if plot_reprojection:
+            for p in reprojection:
+                p_x, p_y = zip(*p)
+                plt.scatter(p_x, p_y)
+                plt.axis('equal')
+                plt.xlim(0, self.w)
+                plt.ylim(0, self.h)
+                plt.show()
+            
+        return reprojection
     
     # Getters for grouped variables
     def focal_lengths(self):
@@ -55,3 +97,111 @@ class Camera:
     
     def image_resolution(self):
         return self.w, self.h
+        
+    def jacobian(self, X, T):
+        
+        def dU_dX(X_c_prime):
+            X_x, X_y, X_z = X_c_prime
+            f_x, f_y = self.f_x, self.f_y
+            return np.array([f_x * ((R_camera[0] * X_z) - (X_x * R_camera[2])), 
+                             f_y * ((R_camera[1] * X_z) - (X_y * R_camera[2]))]) / (X_z ** 2)
+        
+        def dUr_dX(U, U_X):
+            k1, k2, k3 = self.k1, self.k2, self.k3          
+            u_r_corr_X = ((k1 * r_sq_X) +
+                          (k2 * 2 * r_sq * r_sq_X) + 
+                          (k3 * 3 * np.square(r_sq) * r_sq_X))
+            return np.multiply(u_r_corr, U_X) + np.outer(U.T, u_r_corr_X)
+        
+        def dUt_dX(U, U_X):
+            p1, p2 = self.p1, self.p2
+            a = ((U[0]*U_X[1])+(U[1]*U_X[0]))
+            return np.array([(2*p1*a) + (p2*(r_sq_X + (4*U[0]*U_X[0]))),
+                             (p1*(r_sq_X + (4*U[1]*U_X[1]))) + (2*p2*a)])
+    
+    
+    
+        def dU_dR(X_c, X_c_prime):
+            X_x, X_y, X_z = X_c_prime
+            f_x, f_y = self.f_x, self.f_y
+            
+            return np.array([f_x * np.concatenate((X_z * X_c, [0, 0, 0], -X_x * X_c)),
+                             f_y * np.concatenate(([0, 0, 0], X_z * X_c, -X_y * X_c))]) / (X_z ** 2)
+
+        def dUr_dR(U, U_R):
+            k1, k2, k3 = self.k1, self.k2, self.k3          
+            u_r_corr_R = ((k1 * r_sq_R) +
+                          (k2 * 2 * r_sq * r_sq_R) + 
+                          (k3 * 3 * np.square(r_sq) * r_sq_R))
+            return np.multiply(u_r_corr, U_R) + np.outer(U.T, u_r_corr_R)
+        
+        def dUt_dR(U, U_R):
+            p1, p2 = self.p1, self.p2
+            a = ((U[0]*U_R[1])+(U[1]*U_R[0]))
+            return np.array([(2*p1*a) + (p2*(r_sq_R + (4*U[0]*U_R[0]))),
+                             (p1*(r_sq_R + (4*U[1]*U_R[1]))) + (2*p2*a)])
+    
+        def dR_dE():
+            sa, ca = np.sin(T[0]), np.cos(T[0])
+            sb, cb = np.sin(T[1]), np.cos(T[1])
+            sc, cc = np.sin(T[2]), np.cos(T[2])
+
+            return np.array([np.array([-sa*cb, -ca*sb, 0]),
+                             np.array([-(sa*sb*sc)-(ca*cc), ca*cb*sc, (ca*sb*cc)+(sa*sc)]),
+                             np.array([-(sa*sb*cc)+(ca*sc), ca*cb*cc,-(ca*sb*sc)+(sa*cc)]),
+                             np.array([ ca*cb, -sa*sb, 0]),
+                             np.array([ (ca*sb*sc)-(sa*cc), sa*cb*sc, (sa*sb*cc)-(ca*sc)]),
+                             np.array([ (ca*sb*cc)+(sa*sc), sa*cb*cc,-(sa*sb*sc)-(ca*cc)]),
+                             np.array([ 0, -cb, 0]),
+                             np.array([ 0, -sb*sc, cb*cc]),
+                             np.array([ 0, -sb*cc,-cb*sc])])
+    
+        # Compute values used in Jacobian
+        R_camera = Camera.rotation_matrix(T[0], T[1], T[2])
+        T_camera = np.array([T[3], T[4], T[5]])
+        
+        X_c = np.subtract(X, T_camera)
+        X_c_prime = np.matmul(R_camera, X_c)
+        
+        U = np.array([self.f_x * X_c_prime[0] / X_c_prime[2], 
+                      self.f_y * X_c_prime[1] / X_c_prime[2]])
+    
+        r_sq = np.square(np.add(np.square(U[0]), np.square(U[1])))
+        u_r_corr = ((self.k1 * r_sq) + (self.k2 * np.power(r_sq, 2)) + (self.k3 * np.power(r_sq, 3)))
+        
+        
+
+        
+        # Compute the derivatives with respect to the point position
+        U_X = dU_dX(X_c_prime)
+        r_sq_X = 4 * np.multiply(np.dot(U, U), np.dot(U, U_X))
+        Ur_X = dUr_dX(U, U_X)
+        Ut_X = dUt_dX(U, U_X)
+        f_X = U_X + Ur_X + Ut_X; f_X[0] *= self.w; f_X[1] *= self.h
+        
+        # Derivatives with respect to camera position is negative of f_X
+        f_C = -f_X
+        
+        # Derivatives with respect to camera orientation
+        U_R = dU_dR(X_c, X_c_prime)
+        r_sq_R = 4 * np.multiply(np.dot(U, U), np.dot(U, U_R))
+        Ur_R = dUr_dR(U, U_R)
+        Ut_R = dUt_dR(U, U_R)
+        f_R = U_R + Ur_R + Ut_R; f_R[0] *= self.w; f_R[1] *= self.h
+        f_E = np.matmul(f_R, dR_dE())
+        
+        # Concatenate into 1 long jacobian matrix and return
+        Jacobian = np.concatenate([f_X, f_E, f_C], axis=1)
+        return Jacobian
+        
+    # Get camera rotation matrix from Euler angles
+    @staticmethod
+    def rotation_matrix(a, b, c):   
+        ca, sa = np.cos(a), np.sin(a)
+        cb, sb = np.cos(b), np.sin(b)
+        cc, sc = np.cos(c), np.sin(c)
+    
+        # Compute and return the rotation matrix
+        return np.array([[ca*cb, (ca*sb*sc) - (sa*cc), (ca*sb*cc) + (sa*sc)], 
+                         [sa*cb, (sa*sb*sc) + (ca*cc), (sa*sb*cc) - (ca*sc)], 
+                         [  -sb,                cb*sc,                cb*cc]])   
--- src/Reconstruction/reconstruction.py
+++ src/Reconstruction/reconstruction.py
@@ -5,9 +5,13 @@ Created on Sat Jul  4 13:24:47 2020
 @author: Jack
 """
 
+import time
+
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy import optimize
+import autograd as ag
+
 
 from camera_model import Camera
 
@@ -16,127 +20,97 @@ class Bundle_adjuster:
     def __init__(self):
         print("init bundle adjuster")
 
-    
-    # Reproject coordinates from world space to pixel space subject to
-    #  X point world coordinates
-    #  T extrinsic camera properties
-    #  C camera model
-    def reproject(self, X, T, C : Camera, plot_reprojection = False):
-        
-        # Get camera properties for each frame
-        R_camera = Camera.rotation_matrix(T[0], T[1], T[2])
-        T_camera = np.array([T[3], T[4], T[5]])
-        f_x, f_y = C.focal_lengths()
-        k1, k2, k3 = C.radial_distortion_coefficients()
-        p1, p2 = C.tangential_distortion_coefficients()
-        w, h = C.image_resolution()
-        
-        
-        # Translate into camera reference frames
-        X_camera = [np.matmul(R_camera, np.subtract(x, T_camera)) for x in X]
-        #  Cull points too near the camera
-        X_camera = [x for x in X_camera if x[2] > 0.01]
-        
-        
-        # Project onto camera plane
-        u_norm_camera = [np.array([f_x * X_c[0] / X_c[2], 
-                                   f_y * X_c[1] / X_c[2]]) for X_c in X_camera]
-        
-            
-        # Distortions
-        u_norm_camera_r_sq = [np.square(np.add(np.square(u[0]), 
-                                               np.square(u[1]))) for u in u_norm_camera]         
 
-        #  Radial
-        u_radial_corrections = [(k1 * u_r +
-                                 k2 * np.power(u_r, 2) +
-                                 k3 * np.power(u_r, 3)) for u_r in u_norm_camera_r_sq]
-        u_c_r = [np.array([u[0] * u_rad_corr, 
-                           u[1] * u_rad_corr]) for u, u_rad_corr in zip(u_norm_camera, u_radial_corrections)]        
-        #  Tangential
-        u_c_t = [np.array([(2*p1*u[0]*u[1]) + (p2*(r + (2*np.square(u[0])))),
-                           (p1*(r + (2*np.square(u[1])))) + (2*p2*u[0]*u[1])]) for u,r in zip(u_norm_camera,u_norm_camera_r_sq)]
-        #  Apply distortions
-        u_norm_camera = [np.array([u[0] + cr[0] + ct[0],
-                                   u[1] + cr[1] + ct[1]]) for (u, cr, ct) in zip(u_norm_camera, u_c_r, u_c_t)]
-            
-            
-        # Project into pixel plane
-        p_image = [np.array([w * (u_x + 0.5),h * (u_y + 0.5)]) for (u_x, u_y) in u_norm_camera]
+    def corrections(self, P, X, T, C:Camera):
         
-        if plot_reprojection:
-            # Plot points for debugging purposes
-            p_x, p_y = zip(*p_image)
-            plt.scatter(p_x, p_y)
-            plt.axis('equal')
-            plt.xlim(0, w)
-            plt.ylim(0, h)
-            plt.show()
+        # Lambda for reprojection taking concatenated coordinates
+        reprojection = lambda _X: C.project(_X[:3], _X[3:])
+        # Jacobian of reprojection
+        proj_jac = ag.jacobian(reprojection)
 
         
-        return p_image
-    
-    
+#        # Store errors
+#        r = [[np.zeros((1, 2)) for i in range(len(X))] for j in range(len(T))]
+#        
+#        
+#        A = [[np.zeros((2, 6)) for i in range(len(X))] for j in range(len(T))]
+#        B = [[np.zeros((2, 3)) for i in range(len(X))] for j in range(len(T))]
+#
+#        U =  [np.zeros((3, 3)) for i in range(len(T))]
+#        V =  [np.zeros((6, 6)) for j in range(len(X))]
+#        W = [[np.zeros((3, 6)) for i in range(len(X))] for j in range(len(T))]
 
+        # For every point
+        for i, _X in enumerate(X):
+            # For every camera
+            for j, _T in enumerate(T):
 
-    # Error in reprojection subject to 
-    #  P pixel coords
-    #  X world coordinates
-    #  (T, K) camera properties
-    #  (w, h) image size
-    def reprojection_errors(self, P, X, T, C):
-        P_reprojected = [self.reproject(X, T_j, C) for T_j in T]
-        residules = np.subtract(P, P_reprojected)
+                # Get the error vector
+                #r[j][i] = P[j, i] - C.project(_X, _T)
+                
+                # Get the jacobian for position and camera position dependency
+                concatenated_params = np.concatenate((_X, _T))
+                
+                t0 = time.time()
+                J_all = np.array(proj_jac(concatenated_params))
+                
+                t1 = time.time()
+                
+                f_R_ja = C.jacobian(_X, _T)
+                
+                t2 = time.time()
+                
+                print(t1 - t0)
+                print(np.round(J_all, 1))
+                
+                print(t2 - t1)
+                print(np.round(f_R_ja, 1))
+                
+                break
+            break
         
-        x_res, y_res = np.dsplit(residules,2)
-  
-        # Returns MxN array for M cameras and N points
-        new_shape = (len(T), len(X))
-        return np.reshape(x_res, new_shape), np.reshape(y_res, new_shape)
-    
-    
-    def corrections(self, P, X, T, C):
-        # Compute derivatives
-        x_err, y_err = self.reprojection_errors(P, X, T, C)
+#                # Store the jacobian in the 
+#                A[j][i] = J_all.T[:3].T
+#                B[j][i] = J_all.T[3:].T
+#                
+#                
+#                U[j] += np.matmul(A[j][i].T, A[j][i])
+#                V[i] += np.matmul(B[j][i].T, B[j][i])
+#                W[j][i] = np.matmul(A[j][i].T, B[j][i])
+#                
+##                H_pp = np.matmul(J_p.T, J_p)
+##                H_pc = np.matmul(J_p.T, J_c)
+##                H_cc = np.matmul(J_c.T, J_c)
+#        
         
-        optimize.minimize()
-        
-        print(x_err)
-        
+                
         
     
 
 def main():
     ba = Bundle_adjuster()
-    C = Camera(9/16, 1, 0, 0, 0, 0, 0, 1280, 720)
+    C = Camera(9/16, 1, 2, 3, 4, 1, 1, 1280, 720)
     
     number_of_points = 4
-    number_of_cameras = 5
+    number_of_cameras = 3
     
     # Use solutions and reprojection to set a dummy problem
-    _X = [np.array([1,0,2]).T,
-          np.array([0,1,2]).T,
-          np.array([0,0,3]).T,
-          np.array([1,1,4]).T]
+    _X = [np.array([ 0,0,5]).T,
+          np.array([ 0,1,5]).T,
+          np.array([ 1,0,4]).T,
+          np.array([ 1,1,4]).T]
     
-    _T_camera = [[0,     0, 0, 0.0, 0, 0],
-                 [0,  -0.1, 0, 0.2, 0, 0],
-                 [0, -0.15, 0, 0.4, 0, 0],
-                 [0, -0.25, 0, 0.6, 0, 0],
-                 [0,  -0.3, 0, 0.8, 0, 0]]
+    _T_camera = [[0, 0, -0.1, 0.0, 0, 0],
+                 [0, 0, -0.2, 0.2, 0, 0],
+                 [0, 0, -0.4, 0.4, 0, 0]]
     
-    ba.reproject(_X, _T_camera[0], C, True)
+    # Attempt to recover _X, _T, _K without knowing them, from P
+    P = C.reproject_all(_X, _T_camera, False)
     
+    X_guess = [np.array([0., 0., 1.]).T for i in range(number_of_points)]
+    T_camera_guess = [np.array([0.8, 0.5, 0.2, 1, 0, 0.]) for i in range(number_of_cameras)]
+    ba.corrections(P, X_guess, T_camera_guess, C)
     
-#    
-#    # Attempt to recover _X, _T, _K without knowing them from P
-#    P = [ba.reproject(_X, _T_camera, C) for _T_camera in _T_camera]
-#
-#    X_guess = [np.array([0, 0, 1]).T for i in range(number_of_points)]
-#    T_camera_guess = [[0, 0, 0, 0, 0, 0] for i in range(number_of_cameras)]
-#    
-#    ba.corrections(P, X_guess, T_camera_guess, C)
-#    
     
     
 if __name__ == '__main__':
